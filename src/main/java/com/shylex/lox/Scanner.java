b370bce6c0eeee933af3b1d062e40b91737fb128
Alex Dauncey <84759985+Shylexx@users.noreply.github.com>
2022-08-31 00:43:55 +0100

    String and Number scanning/parsing

--- src/main/java/com/shylex/lox/Scanner.java
+++ src/main/java/com/shylex/lox/Scanner.java
@@ -31,6 +31,9 @@ public class Scanner {
         return current >= source.length();
     }
 
+    /**
+     * Advances a character and then parses it into a token based on the character it is or matches
+     */
     private void scanToken() {
         char c = advance();
         switch (c) {
@@ -44,13 +47,130 @@ public class Scanner {
             case '+' -> addToken(PLUS);
             case ';' -> addToken(SEMICOLON);
             case '*' -> addToken(STAR);
+            case '!' -> addToken(match('=') ? BANG_EQUAL : BANG);
+            case '=' -> addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+            case '<' -> addToken(match('=') ? LESS_EQUAL : LESS);
+            case '>' -> addToken(match('=') ? GREATER_EQUAL : GREATER);
+            case '/' -> {
+                // If two slashes, line comment
+                if(match('/')) {
+                    // A comment goes til the end of the line
+                    // Peek next letter and continue skipping letters until end of the line
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                } else {
+                    addToken(SLASH);
+                }
+            }
+            // Skip whitespace
+            case ' ', '\r', '\t' -> {}
+            // Skip newline char, but increment line number
+            case '\n' -> line++;
+
+            // Literals
+
+            case '"' -> string();
+
+            default -> {
+                if (isDigit(c)) {
+                    number();
+                } else {
+                    Lox.error(line, "Unexpected Character.");
+                }
+            }
         }
     }
 
+    /**
+     * Logic for parsing whether the next token is a string literal or not
+     */
+    private void string() {
+        while(peek() != '"' && !isAtEnd()) {
+            if(peek() == '\n') line++;
+            advance();
+        }
+
+        if(isAtEnd()) {
+            Lox.error(line, "Unterminated String.");
+            return;
+        }
+
+        // The closing "
+        advance();
+
+        // Trim the " off of the string value
+        String value = source.substring(start + 1, current - 1);
+        addToken(STRING, value);
+    }
+
+    private void number() {
+        while  (isDigit(peek())) advance();
+
+        // Check that the decimal is not trailing
+        if(peek() == '.' && isDigit(peekNext())) {
+            // Consume the decimal point
+            advance();
+
+            // Parse the second half of the floating point number
+            while (isDigit(peek())) advance();
+        }
+
+        addToken(NUMBER,
+                Double.parseDouble(source.substring(start,current)));
+    }
+
+    /**
+     * Checks if the input is a digit
+     * @param c The input to check
+     * @return true if input is digit, false if not
+     */
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    /**
+     * Matches the next character in the current source and consumes it as well as returning true if the parameter matches it
+     * @param expected The character to compare to
+     * @return Whether the next character in the source matches the parameter.
+     */
+    private boolean match(char expected) {
+        if (isAtEnd()) return false;
+        if (source.charAt(current) != expected) return false;
+
+        // If next character matches param, consume it and return true
+        current++;
+        return true;
+    }
+
+    /**
+     * Checks the next character in the current source file and returns it
+     * @return the next character in the current file
+     */
+    private char peek() {
+        if(isAtEnd()) return '\0';
+        return source.charAt(current);
+    }
+
+    /**
+     * Checks two characters ahead in the current source file and returns the character
+     * @return two characters ahead in current source
+     */
+    private char peekNext() {
+        if(current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
+    /**
+     * Moves forward a character in the source and returns the next character, 'consuming' it.
+     * @return the next character
+     */
     private char advance() {
         return source.charAt(current++);
     }
 
+    /**
+     * Adds a token to the arraylist of tokens
+     * @param type the token to add
+     */
     private void addToken(TokenType type) {
         addToken(type, null);
     }
@@ -59,4 +179,6 @@ public class Scanner {
         String text = source.substring(start, current);
         tokens.add(new Token(type, text, literal, line));
     }
+
+
 }
